ba649119212133cf695a21607c45ef74c3665f8c
Eric Renfro <psi-jack@linux-help.org>
2025-08-29 19:22:46 -0400

    Initial commit, and Quartz configured

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Linux-Help",
+    pageTitleSuffix: "Techni-Wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "erenfro.github.io/linux-help",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
